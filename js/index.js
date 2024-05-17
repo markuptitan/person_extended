fac6a294ce9594f904f90f464fddd92e821b1daa
markuptitan <samsonlukhele76@gmail.com>
2024-05-17 12:34:00 +0200

    Fix some issues with printing out interests

--- js/index.js
+++ js/index.js
@@ -1,19 +1,13 @@
 function separateLines(textarea) {
   let text = textarea.value;
   let lines = text.split("\n");
-  let wordsInLines = [];
-  lines.forEach((line) => {
-    let words = line.trim().split(" ");
-    words = words.filter((word) => word !== "");
-    if (words.length > 0) {
-      wordsInLines.push(words);
-    }
-  });
-  return wordsInLines;
+  let cleanedLines = lines
+    .map((line) => line.trim())
+    .filter((line) => line !== "");
+  return cleanedLines;
 }
 
 function personFormSubmit() {
-  
   let targetForm = document.querySelector("#mainForm");
   let nameInput = targetForm.querySelector("#name");
   let ageInput = targetForm.querySelector("#age");
@@ -23,10 +17,8 @@ function personFormSubmit() {
   let age = ageInput.value;
   let gender = genderInput.value;
   let interests = separateLines(interestInput);
-  if (
-    nameInput.value.trim() === "" ||
-    ageInput.value.trim() === ""
-  ) {
+
+  if (name.trim() === "" || age.trim() === "") {
     alert("Please fill out all required fields.");
     return false;
   }
